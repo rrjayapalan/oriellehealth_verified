e227a608adc6d33d7e99fa4b2f68aea5fee0a9be
rrjayapalan <rrjayapalan@gmail.com>
2025-11-30 01:37:45 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
             <img
               src="/logo.png"
               alt="Orielle Health Services Logo"
-              className="h-10 md:h-12 w-auto"
+              className="h-12 md:h-14 w-auto"
             />
           </div>
 
